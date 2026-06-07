66e42d5092124c083552f0851fbf4d6bcd35cf9a
sidmohap <sidhant.mohapatra2@gmail.com>
2026-06-07 11:51:26 +0530

    initial commit

---
+++ src/main/java/com/sid/docker/dockerpublish/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.sid.docker.dockerpublish.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to Docker Publish Application!";
+    }
+}
